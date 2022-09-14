9aa19c775882251a997a41a71dd3c40380ae7dd2
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-09-13 21:45:52 -0700

    add delete

--- todo-list/src/components/body/Todo.jsx
+++ todo-list/src/components/body/Todo.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { Button, Form } from 'react-bootstrap'
+import { FcFullTrash } from 'react-icons/fc'
+import { FiEdit } from 'react-icons/fi'
 
 import BGIMG from '../../assets/bg-note.jpg'
 import './todo.css'
@@ -7,31 +9,57 @@ import './todo.css'
 const Todo = () => {
   const [input, setInput] = useState('')
   const [todoList, setTodoList] = useState([])
+  const ref = useRef(null)
 
   const handleChange = (e) => {
     setInput(e.target.value)
   }
 
-  const ref = useRef(null)
-
   useEffect(() => {
     ref.current.focus()
-  })
+  }, [])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = useCallback((e) => {
     e.preventDefault()
     
+    const previousTodoList = [...todoList]
+
     if (input === "") {
       return
     } else {
-      setTodoList([
-        ...todoList, 
-        { id: Math.floor(Math.random() * 10000),
-          text: input
-        }
-      ])
+      const newTodo = {
+        id: Math.floor(Math.random() * 10000),
+        text: input,
+        completed: false
+      }
+      const newTodoList = previousTodoList.concat(newTodo)
+      setTodoList(newTodoList)
     }
     setInput('')
+    
+  }, [todoList, input])
+
+  const handleChecked = useCallback((id) => {
+    const previousTodoList = [...todoList]
+    const newTodoList = previousTodoList.map((todo) => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodoList(newTodoList)
+  }, [todoList])
+
+  const handleDeleteTodo = useCallback((id) => {
+    const previousTodoList = [...todoList]
+    const newTodoList = previousTodoList.filter((todo) => todo.id !== id)
+
+    setTodoList(newTodoList)
+  }, [todoList])
+
+  const handleEditTodo = () => {
+
   }
 
   return (
@@ -45,7 +73,30 @@ const Todo = () => {
         </Form>
         
         <div className='container__todo-list'>
-          <img src={BGIMG} alt="" className="bg-img"/>
+          <div className='d-flex justify-content-center align-items-start'>
+            <img src={BGIMG} alt="" className="position-absolute bg-img"/>
+          </div>
+          
+          <div className='temp__box'>
+            <Form>
+              {
+                todoList.map(todo => (
+                  <div key={ `${todo.id}` } className='d-flex mb-2'>
+                    <Form.Check 
+                      type='checkbox'
+                      checked={todo.completed}
+                      onChange={() => handleChecked(todo.id)}
+                      label={`${todo.text}`}
+                      style={{'width': 300}}
+                    />
+                    <FiEdit className='me-2' onClick={ handleEditTodo() }/>
+                    <FcFullTrash onClick={ handleDeleteTodo(todo.id) }/>
+                    
+                  </div>
+                ))
+              }
+            </Form>
+          </div>
         </div>
       </div>
       
