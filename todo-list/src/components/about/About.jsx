1db60dfa88d277a5f9c7afd1eae3a748653d5501
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-09-27 22:15:00 -0700

    about page

--- todo-list/src/components/about/About.jsx
+++ todo-list/src/components/about/About.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+import { ImEarth } from 'react-icons/im'
+
 import './about.css'
 import ME from '../../assets/ryan.jpg'
 
 const About = () => {
   return (
     <div className='d-flex flex-column align-items-center about__container'>
-      <div className='mb-5 text-center about__banner'>
-        <p>About This App</p>
+      <div className='d-flex justify-content-center align-items-end mb-2 text-center about__banner'>
+        <h3 style={{'color':'white', 'fontWeight':'bolder'}}>About This App</h3>
       </div>
 
-      <div className='d-flex flex-column justify-content-start align-items-center info__container'>
+      <div className='d-flex flex-column justify-content-start align-items-center w-100 h-100 info__container'>
         <div className='mt-5 mb-2 pt-5'>
           <img src={ME} alt="" className='img-me' />
         </div>
 
         <div className='mt-5 mb-5'>
-          <p>This is a Todo List </p>
+          <p>This is a React tutorial built in the Github Repository.</p>
         </div>
         
-        <div className='mt-5 mb-5'>
-          <div>Github</div>
-          <div>WebPage</div>
+        <div className='d-grid gap-4'>
+          <a href="https://github.com/ryancho-hakhyeon/ToDo-list" target="_blank" rel="noreferrer">
+            <div style={{ width:300 , backgroundColor:'black', borderRadius:20 }} className='d-flex justify-content-center align-items-center'>
+              <BsGithub className='me-2'/>
+              <span>Github</span>
+            </div>
+          </a>
+
+          <a href="https://ryanchoportfolio.netlify.app/" target="_blank" rel="noreferrer">
+            <div style={{width:300, backgroundColor:'purple', borderRadius:20}} className='d-flex justify-content-center  align-items-center'>
+              <ImEarth className='me-2'/>
+              <span>WebPage</span>
+            </div>
+          </a>
         </div>
       </div>
     </div>
--- todo-list/src/components/header/NavSide.jsx
+++ todo-list/src/components/header/NavSide.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 import { Offcanvas, Nav, Button } from 'react-bootstrap'
 import { FaBars } from 'react-icons/fa'
+import { BiTask } from 'react-icons/bi'
+import { FaRegQuestionCircle } from 'react-icons/fa'
 
 import './navside.css'
 
@@ -18,12 +20,19 @@ const NavSide = () => {
 
       <Offcanvas show={open} onHide={showSidebar}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title><h1 className='ms-2'>TODO LIST</h1></Offcanvas.Title>
+          <Offcanvas.Title>
+            <h1 className='ms-2'>TODO LIST</h1>
+            </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Nav className="justify-content-end flex-column pe-3 mb-2">
-            <Nav.Link href='/'>Todo List</Nav.Link>
-            <Nav.Link href='/About'>About</Nav.Link>
+          <Nav className="d-flex flex-column pe-3 mb-2">
+            <Nav.Link href='/'>
+              <BiTask className='me-3' /> Todo List
+            </Nav.Link>
+            
+            <Nav.Link href='/About'>
+              <FaRegQuestionCircle className='me-3' /> About
+            </Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
